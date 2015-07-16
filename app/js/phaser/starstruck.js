bc6d09965bc057f860c79f9690157d0c4f7a5648
clement-95 <bourdinclement@yahoo.fr>
2015-07-16 15:44:37 +0200

    Game generator

--- app/js/phaser/starstruck.js
+++ app/js/phaser/starstruck.js
@@ -144,3 +144,147 @@ angular.element(document.body).scope().launch_phaser_starstruck = function (surv
 
     }
 }
+
+function launch_phaser_editor (plateforms) {
+    $('#phaser-starstruck').html(' ');
+    window.game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-starstruck', { preload: preload, create: create, update: update, render: render });
+
+    function preload() {
+        game.load.spritesheet('pl1', 'img/game/arrivee.png');
+        game.load.spritesheet('pl2', 'img/game/nuage.png');
+        game.load.spritesheet('pl3', 'img/game/plateforme2.png');
+        game.load.spritesheet('pl4', 'img/game/plateforme3.png');
+
+        game.load.spritesheet('dude', 'js/phaser/assets/games/starstruck/dude.png', 32, 48);
+        game.load.image('background', 'img/game/fond.png');
+    }
+
+    var map;
+    var tileset;
+    var layer;
+    var player;
+    var facing = 'left';
+    var jumpTimer = 0;
+    var cursors;
+    var jumpButton;
+    var bg;
+
+    function create() {
+
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        game.stage.backgroundColor = '#000000';
+
+        bg = game.add.tileSprite(0, 0, 800, 600, 'background');
+        bg.fixedToCamera = true;
+
+        game.platforms = game.add.physicsGroup();
+
+        plateforms.forEach(function(pl) {
+            alert(pl.img.src);
+            if (pl.img.src.includes('img/game/arrivee.png')) {
+                var newPlat = game.platforms.create(pl.imageX, pl.imageY, 'pl1');
+            }
+            if (pl.img.src.includes('img/game/nuage.png')) {
+                var newPlat = game.platforms.create(pl.imageX, pl.imageY, 'pl2');
+            }
+            if (pl.img.src.includes('img/game/plateforme2.png')) {
+                var newPlat = game.platforms.create(pl.imageX, pl.imageY, 'pl3');
+            }
+            if (pl.img.src.includes('img/game/plateforme3.png')) {
+                var newPlat = game.platforms.create(pl.imageX, pl.imageY, 'pl4');
+            }
+        });
+
+        game.platforms.setAll('body.allowGravity', false);
+        game.platforms.setAll('body.immovable', true);
+        game.platforms.setAll('body.velocity.x', 0);
+
+        game.physics.arcade.gravity.y = 800;
+
+        player = game.add.sprite(32, 32, 'dude');
+        game.physics.enable(player, Phaser.Physics.ARCADE);
+
+        player.body.bounce.y = 0.2;
+        player.body.collideWorldBounds = true;
+        player.body.setSize(20, 32, 5, 16);
+
+        player.animations.add('left', [0, 1, 2, 3], 10, true);
+        player.animations.add('turn', [4], 20, true);
+        player.animations.add('right', [5, 6, 7, 8], 10, true);
+        
+        game.camera.follow(player);
+
+        cursors = game.input.keyboard.createCursorKeys();
+        jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    }
+
+    function update() {
+
+        game.physics.arcade.collide(player, layer);
+
+        player.body.velocity.x = 0;
+
+        if (cursors.left.isDown)
+        {
+            player.body.velocity.x = -150;
+
+            if (facing != 'left')
+            {
+                player.animations.play('left');
+                facing = 'left';
+            }
+        }
+        else if (cursors.right.isDown)
+        {
+            player.body.velocity.x = 150;
+
+            if (facing != 'right')
+            {
+                player.animations.play('right');
+                facing = 'right';
+            }
+        }
+        else
+        {
+            if (facing != 'idle')
+            {
+                player.animations.stop();
+
+                if (facing == 'left')
+                {
+                    player.frame = 0;
+                }
+                else
+                {
+                    player.frame = 5;
+                }
+
+                facing = 'idle';
+            }
+        }
+        
+        if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
+        {
+            player.body.velocity.y = -500;
+            jumpTimer = game.time.now + 750;
+        }
+        
+        //  Run collision
+        this.physics.arcade.collide(player, game.platforms, null, null, game);
+    }
+
+    function render () {
+
+        // game.debug.text(game.time.physicsElapsed, 32, 32);
+        // game.debug.body(player);
+        // game.debug.bodyInfo(player, 16, 24);
+
+    }
+    
+    function collisionHandler () {
+
+        
+
+    }
+}
