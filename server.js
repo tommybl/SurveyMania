6eb48e3737d1344aa02c73b009ae48e7ede0da4f
clement-95 <bourdinclement@yahoo.fr>
2015-11-04 15:18:13 +0100

    Fix step 1 on 2

--- server.js
+++ server.js
@@ -41,8 +41,6 @@ var transporter = nodemailer.createTransport({
     }
 });
 
-var captchaSecret = "6LdYQRATAAAAAH7D2VE5phz7E5X8DtDISZ_6wYY7";
-
 var JsonFormatter = {
     stringify: function (cipherParams) {
         return cipherParams.ciphertext.toString(CryptoJS.enc.Base64) + cipherParams.salt.toString();
@@ -62,6 +60,13 @@ var JsonFormatter = {
 
 // We need our own "html_special_chars" function
 function escapeHtml(text) {
+    if (text == null)
+        return null;
+    if (text == "")
+        return "";
+    if (!isNaN(text))
+        return text;
+
   var map = {
     '&': '\&',
     '<': '\<',
@@ -69,7 +74,6 @@ function escapeHtml(text) {
     '"': '\"',
     "'": "\'\'"
   };
-
   return (text != null) ? text.replace(/[&<>"']/g, function(m) { return map[m]; }) : null;
 }
 
@@ -99,29 +103,6 @@ app
     res.render('partials/createSurvey');
 })
 
-.post('/app/validateCaptcha', function (req, res) {
-    res.setHeader('Content-Type', 'application/json; charset=UTF-8');
-    res.setHeader('Accept', 'application/json');
-
-    if (req.body.captcha == undefined) {
-        res.status(500).json({code: 500, error: "Internal server error", message: "Invalid captcha request"});
-        return;
-    }
-
-    request.post(
-        'https://www.google.com/recaptcha/api/siteverify',
-        { form: { secret: captchaSecret, response: req.body.captcha } },
-        function (error, response, body) {
-            body = JSON.parse(body);
-            if (error == null && response.statusCode == 200 && body.success == true) {
-                res.status(200).json({code: 200, success: true});
-            }
-            else res.status(200).json({code: 200, success: false, body: body});
-        }
-    );
-
-})
-
 .post('/app/account/admin/validate/survey', function (req, res) {
     res.setHeader('Content-Type', 'application/json; charset=UTF-8');
     res.setHeader('Accept', 'application/json');
@@ -135,13 +116,6 @@ app
     var description = (req.body.description == null || req.body.description == "") ? "NULL" : escapeHtml(req.body.description);
     var category = req.body.category;
     var dateNow = escapeHtml(moment().format("YYYY-MM-DD HH:mm:ss"));
-    console.log(name);
-    console.log(points);
-    console.log(instructions);
-    console.log(description);
-    console.log(category);
-    console.log(sections);
-    console.log(questions);
 
     var getInputType = function (num) {
         if (num == '0') return 5;
@@ -158,7 +132,7 @@ app
         if(err) {console.log("err1"); res.status(500).json({code: 500, error: "Internal server error", message: "Error fetching client from pool"});}
         else {
             var query = 'INSERT INTO surveymania.survey_headers(organization_id, category_id, theme_id, name, instructions, info, points, publied, publication_date) ' +
-                        'VALUES (' + req.user.organization + ', ' + category + ', 1, \'' + name + '\', \'' + instructions + '\', \'' + description + '\', ' + points + ', FALSE, NULL) ' +
+                        'VALUES (' + req.user.organization + ', ' + escapeHtml(category) + ', 1, \'' + escapeHtml(name) + '\', \'' + escapeHtml(instructions) + '\', \'' + escapeHtml(description) + '\', ' + escapeHtml(points) + ', FALSE, NULL) ' +
                         'RETURNING id';
             client.query(query, function(err, result) {
                 done();
@@ -170,7 +144,7 @@ app
                         function (se, callback0) {
                             var sect = se;
                             var query = 'INSERT INTO surveymania.survey_sections(header_id, title, required, section_order) ' +
-                                        'VALUES (' + header_id + ', \'' + sect.title + '\', ' + sect.required + ', ' + (section_index+1) + ') ' +
+                                        'VALUES (' + header_id + ', \'' + escapeHtml(sect.title) + '\', ' + sect.required + ', ' + (section_index + 1) + ') ' +
                                         'RETURNING id';
                             client.query(query, function(err, result) {
                                 done();
@@ -200,7 +174,7 @@ app
                                                 var question = q;
 
                                                 var query = 'INSERT INTO surveymania.questions(survey_section_id, input_type_id, description, question_order, multiple_answers) ' +
-                                                            'VALUES (' + section_id + ', ' + getInputType(question.type) + ', \'' + escapeHtml((question.type == '7') ? question.text : question.title) + '\', ' + (question_index+1) + ', FALSE) ' +
+                                                            'VALUES (' + section_id + ', ' + getInputType(question.type) + ', \'' + escapeHtml((question.type == '7') ? question.text : question.title) + '\', ' + (question_index + 1) + ', FALSE) ' +
                                                             'RETURNING id';
                                                 client.query(query, function(err, result) {
                                                     done();
@@ -253,7 +227,7 @@ app
                                                             else {
                                                                 var query = 'INSERT INTO surveymania.option_choices(question_id, choice_name, option_order, linked_section_id) VALUES ';
                                                                 for (var k = 0; k < question.option.length; k++) {
-                                                                    query += '(' + question_id + ', \'' + question.option[k] + '\', ' + (k+1) + ', NULL)';
+                                                                    query += '(' + question_id + ', \'' + escapeHtml(question.option[k]) + '\', ' + (k + 1) + ', NULL)';
                                                                     if (k != question.option.length - 1) query += ', ';
                                                                 }
                                                             }
@@ -270,7 +244,7 @@ app
                                                             else {
                                                                 var query = 'INSERT INTO surveymania.option_choices(question_id, choice_name, option_order, linked_section_id) VALUES ';
                                                                 for (var k = 0; k < question.option.length; k++) {
-                                                                    query += '(' + question_id + ', \'' + question.option[k].title + '\', ' + (k+1) + ', ' + sections_details[question.option[k].sectionId].sid + ')';
+                                                                    query += '(' + question_id + ', \'' + escapeHtml(question.option[k].title) + '\', ' + (k+1) + ', ' + sections_details[question.option[k].sectionId].sid + ')';
                                                                     if (k != question.option.length - 1) query += ', ';
                                                                 }
                                                             }
@@ -283,12 +257,12 @@ app
                                                         if (question.video.length > 0 || question.image.length > 0) {
                                                             var query = 'INSERT INTO surveymania.question_medias(question_id, media_path, media_order, media_type, description) VALUES ';
                                                             for (var k = 0; k < question.image.length; k++) {
-                                                                query += '(' + question_id + ', \'' + question.image[k].url + '\', ' + (k+1) + ', \'image_url\', \'' + question.image[k].description + '\')';
+                                                                query += '(' + question_id + ', \'' + escapeHtml(question.image[k].url) + '\', ' + (k + 1) + ', \'image_url\', \'' + escapeHtml(question.image[k].description) + '\')';
                                                                 if (k != question.image.length - 1) query += ', ';
                                                             }
                                                             for (var k = 0; k < question.video.length; k++) {
                                                                 if (k == 0 && question.image.length > 0) query += ', ';
-                                                                query += '(' + question_id + ', \'' + question.video[k].url + '\', ' + (k+1) + ', \'youtube\', \'' + question.video[k].description + '\')';
+                                                                query += '(' + question_id + ', \'' + escapeHtml(question.video[k].url) + '\', ' + (k + 1) + ', \'youtube\', \'' + escapeHtml(question.video[k].description) + '\')';
                                                                 if (k != question.video.length - 1) query += ', ';
                                                             }
                                                             client.query(query, function(err, result) {
@@ -318,7 +292,7 @@ app
                                             console.log("err19"); 
                                             res.status(500).json({code: 500, error: "Internal server error", message: "Error running query"});
                                         } else {
-                                            res.status(200).json({code: 200, message: "Survey successfully created"});
+                                            res.status(200).json({code: 200, message: "Survey successfully created", survey_id: header_id});
                                         }
                                     }
                                 );
@@ -1544,7 +1518,7 @@ app
             else {
                 var query = 'SELECT sh.name AS header_name'
                     + ' FROM surveymania.survey_headers sh'
-                    + ' WHERE sh.id = ' + surveyid + ' AND sh.organization_id = ' + user.organization + ' AND sh.stopped = true';
+                    + ' WHERE sh.id = ' + surveyid + ' AND sh.organization_id = ' + user.organization;
                 client.query(query, function(err, result) {
                     done();
                     if (err) res.status(500).json({code: 500, error: "Internal server error", message: "Error running query"});
